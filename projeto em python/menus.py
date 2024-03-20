767e6ddbfd84c5d9cc38b0e64346dffe81d64a99
Pedro Lucas <pedroluuucas777@gmail.com>
2024-03-20 09:11:12 -0300

    Add: exibe_logo, exibe_opcao, adiciona_subtitulo, volta_ao_menu, opcao_invalida

---
+++ projeto em python/menus.py
@@ -0,0 +1,46 @@
+import os
+import main
+import animais
+
+def exibe_logo():
+    print('''
+        ██████╗░███████╗██████╗░██████╗░░█████╗░░██████╗
+        ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝
+        ██████╔╝█████╗░░██║░░██║██████╔╝██║░░██║╚█████╗░
+        ██╔═══╝░██╔══╝░░██║░░██║██╔══██╗██║░░██║░╚═══██╗
+        ██║░░░░░███████╗██████╔╝██║░░██║╚█████╔╝██████╔╝
+        ╚═╝░░░░░╚══════╝╚═════╝░╚═╝░░╚═╝░╚════╝░╚═════╝░
+        
+        ██╗░░░██╗███████╗████████╗
+        ██║░░░██║██╔════╝╚══██╔══╝
+        ╚██╗░██╔╝█████╗░░░░░██║░░░
+        ░╚████╔╝░██╔══╝░░░░░██║░░░
+        ░░╚██╔╝░░███████╗░░░██║░░░
+        ░░░╚═╝░░░╚══════╝░░░╚═╝░░░
+          ''')
+
+def exibe_opçao():
+    print('1. Adiciona tutor')
+    print('2. Remover tutor')
+    print('3. Adiciona animal')
+    print('4. Remover animal')
+    print('5. Editar informação de animal')
+    print('6. Buscar animal por nome')
+    print('7. Listar tutores e seu animais')
+    print('8. Sair')
+
+def adiciona_subtitulo(texto):
+    os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
+    print(texto)
+    print(linha)
+    print()
+
+def volta_ao_menu():
+    input('\nDigite uma tecla para voltar ao menu')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    volta_ao_menu()
